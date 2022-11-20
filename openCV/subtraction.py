fe34957256b3b62cd79538539daf85bb7d0a6043
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-11-21 00:14:08 +0530

    Create subtraction.py

---
+++ openCV/subtraction.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+img1 = cv.imread("images/Lenna.png", 0)
+img2 = cv.imread("images/LennaNew.png", 0)
+cv.imshow("Image1", img1)
+
+
+for i in range(len(img1)):
+    for j in range(len(img1[0])):
+        if img1[i][j] > img2[i][j]:
+            img1[i][j] = img1[i][j] - img2[i][j]
+        else:
+            img1[i][j] = img2[i][j] + img1[i][j]
+
+cv.imshow("Image3", img1)
+cv.imshow("Image2", img2)
+
+
+cv.waitKey(0)
