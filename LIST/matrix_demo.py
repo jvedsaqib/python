1082fdfaefe956b379de0f32fce771299be479f3
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-12-08 22:32:09 +0530

    Create matrix_demo.py

---
+++ LIST/matrix_demo.py
@@ -0,0 +1,20 @@
+import matrix as m
+
+
+#print_matrix(input_matrix(3,3))
+
+'''
+# Addition of two matrices
+
+print("matrix1: ")
+mat1 = m.input_matrix(2,2)
+print("matrix2: ")
+mat2 = m.input_matrix(2,2)
+
+m.print_matrix(mat1)
+print()
+m.print_matrix(mat2)
+print()
+m.print_matrix(m.add(mat1, mat2))
+
+'''
