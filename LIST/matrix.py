53aaaa7cebde9614c2dd9b82f747e9b5af150182
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-12-08 22:31:01 +0530

    Update matrix.py

--- LIST/matrix.py
+++ LIST/matrix.py
@@ -1,4 +1,4 @@
-# input a matrix and print it
+# input a matrix
 
 def input_matrix(row_len, col_len):
     mat = []
@@ -12,12 +12,26 @@ def input_matrix(row_len, col_len):
         print()
     return mat
 
+# print a matrix
 
 def print_matrix(mat):
     for i in range(len(mat)):
         for j in range(len(mat[0])):
             print(mat[i][j], end=" ")
         print()
+
+# addition of two matrix
+
+def add(mat1, mat2):
+    res_mat = []
+    for i in range(len(mat1)):
+        res_mat_col = []
+        for j in range(len(mat2)):
+            temp = mat1[i][j] + mat2[i][j]
+            res_mat_col.append(temp)
+        res_mat.append(res_mat_col)
+    
+    return res_mat
 
 
 #print_matrix(input_matrix(3,3))
