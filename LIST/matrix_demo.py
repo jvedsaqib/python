0c66456e18714cfa70481f5a0d72b51424bcbaee
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-12-09 17:18:36 +0530

    Update matrix_demo.py

--- LIST/matrix_demo.py
+++ LIST/matrix_demo.py
@@ -18,3 +18,19 @@ print()
 m.print_matrix(m.add(mat1, mat2))
 
 '''
+
+'''
+# Subtraction of two matrices
+
+print("matrix1: ")
+mat1 = m.input_matrix(2,2)
+print("matrix2: ")
+mat2 = m.input_matrix(2,2)
+
+m.print_matrix(mat1)
+print()
+m.print_matrix(mat2)
+print()
+m.print_matrix(m.sub(mat1, mat2))
+
+'''
