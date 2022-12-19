f367a463ffc56b08808e84f364835caec4987c28
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-12-19 17:01:35 +0530

    Create bubble_sort.py

---
+++ Sort_Search/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+print(bubble_sort([2,1,3,4,5]))     # 1 2 3 4 5
